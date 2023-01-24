524e9211911c04ee709a8cd892132fc2d72114be
anon <>
2023-01-24 10:04:28 +0100

    some fixes

--- intt.hpp
+++ intt.hpp
@@ -1038,14 +1038,7 @@ INTT_TYPE_PROMOTION__(-)
 INTT_TYPE_PROMOTION__(*)
 INTT_TYPE_PROMOTION__(/)
 INTT_TYPE_PROMOTION__(<=>)
-
-template <typename A, std::size_t M, typename B,
-  std::size_t N, enum feat ...F, enum feat ...G>
-constexpr auto operator==(intt<A, M, F...> const& a,
-  intt<B, N, G...> const& b) noexcept
-{
-  return a <=> b == 0;
-}
+INTT_TYPE_PROMOTION__(==)
 
 // conversions
 #define INTT_LEFT_CONVERSION__(OP)\
