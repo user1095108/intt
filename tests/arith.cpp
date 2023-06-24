9e43b75733a7a6ec156ecc25bc5efc01adb6272a
user1095108 <janezz55@gmail.com>
2023-06-24 07:54:32 +0200

    some fixes

---
+++ tests/arith.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include "../arith.hpp"
+
+int main()
+{
+  using v4ui = std::uint32_t __attribute__ ((vector_size (4 * sizeof(std::uint32_t))));
+
+  v4ui a{1, 0, 0, 0};
+  v4ui b{1, 0, 0, 0};
+
+  ar::neg(a);
+  ar::not_(a);
+
+  ar::add(a, b);
+  ar::sub(a, b);
+
+  return 0;
+}
