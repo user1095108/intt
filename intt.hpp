46b16b593c820e4414e7139acfd74631de35be27
Janez Žemva <janezz55@gmail.com>
2026-08-11 18:34:32 +0200

    some fixes

--- intt.hpp
+++ intt.hpp
@@ -745,7 +745,7 @@ struct hash<U>
     return [&]<auto ...I>(std::size_t h, std::index_sequence<I...>)
       noexcept(noexcept(std::hash<T>()(std::declval<T const&>())))
       {
-        return ((h = intt::detail::mix(h + std::hash<T>()(a[I + 1]))), ...), h;
+        return ((h = intt::detail::mix(h + std::hash<T>()(a[I + 1]))),...), h;
       }(intt::detail::mix(std::hash<T>()(a[0])),
         std::make_index_sequence<U::words - 1>());
   }
