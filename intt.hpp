c7759b560aae31e812998bb17fbc0e42be778f5d
Janez Žemva <janezz55@gmail.com>
2026-08-12 00:30:38 +0200

    some fixes

--- intt.hpp
+++ intt.hpp
@@ -87,13 +87,15 @@ constexpr std::uint64_t ror64(std::uint64_t x, int k) noexcept {
 
 constexpr std::size_t mix(std::size_t x) noexcept
 {
-  if constexpr(sizeof(std::size_t) >= 8)
+  static_assert(sizeof(std::size_t) == 4 || sizeof(std::size_t) == 8);
+
+  if constexpr(sizeof(std::size_t) == 8)
   { // NASAM
     x ^= ror64(x, 25) ^ ror64(x, 47); x *= std::size_t(0x9E6C63D0676A9A99ULL);
     x ^= x >> 23 ^ x >> 51; x *= std::size_t(0x9E6D62D06F6A9A9BULL);
     return x ^ (x >> 23) ^ (x >> 51);
   }
-  else // 32-bit std::size_t
+  else
   { // lowbias32
     x ^= x >> 16; x *= std::size_t(0x7feb352dU);
     x ^= x >> 15; x *= std::size_t(0x846ca68bU);
