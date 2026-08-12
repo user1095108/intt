3f0a1e11bd9b21ba7910e108093417d46d61dfb8
Janez Žemva <janezz55@gmail.com>
2026-08-13 01:15:51 +0200

    some fixes

--- intt.hpp
+++ intt.hpp
@@ -81,23 +81,18 @@ consteval std::size_t num_digits(std::size_t const N) noexcept
   return N / 3 + !!(N % 3); // 2^N <= 8^J, N <= 3*J
 }
 
-constexpr std::size_t mix(std::size_t x) noexcept
-{
-  static_assert(sizeof(x) == sizeof(std::uint32_t) ||
-    sizeof(x) == sizeof(std::uint64_t));
+constexpr std::size_t mix(std::uint64_t x) noexcept
+{ // moremur
+  x = (x ^ (x >> 27)) * std::uint64_t(0x3C79AC492BA7B653ULL);
+  x = (x ^ (x >> 33)) * std::uint64_t(0x1C69B3F74AC4AE35ULL);
+  return x ^ (x >> 27);
+}
 
-  if constexpr(sizeof(x) == sizeof(std::uint64_t))
-  { // moremur
-    x = (x ^ (x >> 27)) * std::size_t(0x3C79AC492BA7B653ULL);
-    x = (x ^ (x >> 33)) * std::size_t(0x1C69B3F74AC4AE35ULL);
-    return x ^ (x >> 27);
-  }
-  else
-  { // lowbias32
-    x = (x ^ (x >> 16)) * std::size_t(0x7feb352dU);
-    x = (x ^ (x >> 15)) * std::size_t(0x846ca68bU);
-    return x ^ (x >> 16);
-  }
+constexpr std::size_t mix(std::uint32_t x) noexcept
+{ // lowbias32
+  x = (x ^ (x >> 16)) * std::uint32_t(0x7feb352dU);
+  x = (x ^ (x >> 15)) * std::uint32_t(0x846ca68bU);
+  return x ^ (x >> 16);
 }
 
 }
