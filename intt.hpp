169a9fdffcd455d079b4ec676d303ecc92728c54
Janez Žemva <janezz55@gmail.com>
2026-08-12 18:46:09 +0200

    some fixes

--- intt.hpp
+++ intt.hpp
@@ -87,15 +87,15 @@ constexpr std::size_t mix(std::size_t x) noexcept
     sizeof(x) == sizeof(std::uint64_t));
 
   if constexpr(sizeof(x) == sizeof(std::uint64_t))
-  { // splitmix64
-    x = x ^ (x >> 30) * std::size_t(0xbf58476d1ce4e5b9ULL);
-    x = x ^ (x >> 27) * std::size_t(0x94d049bb133111ebULL);
-    return x ^ (x >> 31);
+  { // moremur
+    x = (x ^ (x >> 27)) * std::size_t(0x3C79AC492BA7B653ULL);
+    x = (x ^ (x >> 33)) * std::size_t(0x1C69B3F74AC4AE35ULL);
+    return x ^ (x >> 27);
   }
   else
   { // lowbias32
-    x = x ^ (x >> 16) * std::size_t(0x7feb352dU);
-    x = x ^ (x >> 15) * std::size_t(0x846ca68bU);
+    x = (x ^ (x >> 16)) * std::size_t(0x7feb352dU);
+    x = (x ^ (x >> 15)) * std::size_t(0x846ca68bU);
     return x ^ (x >> 16);
   }
 }
